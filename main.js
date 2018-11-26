97e310d2579124e6536916969e1af394566d47cd
Ian <ianhawe24@hotmail.co.uk>
2018-11-26 18:02:49 +0000

    while loop added

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
+
 var random = 0;
-
-var num1 = prompt("Hello please enter the first number!");
-var num2 = prompt("Please enter your second number!");
-var symbol = prompt("Please enter the operator you wish to do");
-num1 = parseInt(num1);
-num2 = parseInt(num2);
-var result;
-
-switch (symbol) {
-
-    case "/":
-        result = num1 / num2;
-        alert(result);
-        break;
-    case "-":
-        result = num1 - num2;
-        alert(result);
-        break;
-    case "*":
-        result = num1 * num2;
-        alert(result);
-        break;
-    default:
-        result = num1 + num2;
-        alert(result);
-        break;
-
-}
-
-//True statement
 while (random === 0) {
-    var = words + random
+    var num1 = prompt("Hello please enter the first number!");
+    var num2 = prompt("Please enter your second number!");
+    var symbol = prompt("Please enter the operator you wish to do");
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    var result;
+
+    switch (symbol) {
+
+        case "/":
+            result = num1 / num2;
+            alert(result);
+            break;
+        case "-":
+            result = num1 - num2;
+            alert(result);
+            break;
+        case "*":
+            result = num1 * num2;
+            alert(result);
+            break;
+
+        default:
+            result = num1 + num2;
+            alert(result);
+
+            break;
+
+    }
+
 
 }
